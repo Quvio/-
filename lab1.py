f6cf7b6cf244053cce54c16c6ce11239076a70d1
Quvio <73601864+Quvio@users.noreply.github.com>
2024-02-24 08:51:50 +0400

    lab1.py

--- lab1.py
+++ lab1.py
@@ -14,7 +14,7 @@ if not os.path.isfile("text.txt"):
 slv = {"0":"ноль", "1":"один", "2":"два", "3":"три", "4":"четыре", "5":"пять", "6":"шесть", "7":"семь", "8":"восемь", "9":"девять"}
 flag = 1
 with open ("text.txt", 'r') as f:
-    line = f.readline()
+    line = f.read()
     nmbrs = line.split()
 
     print("Результат кода:")
