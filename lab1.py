0e46ed20f63e9df91357f285c942d410fedfc8b4
Quvio <73601864+Quvio@users.noreply.github.com>
2024-02-24 09:05:56 +0400

    lab1.py

--- lab1.py
+++ lab1.py
@@ -8,32 +8,33 @@
 Четные двоичные числа, не превышающие 102410, в которых встречается больше чем одна серия из трех подряд идущих нуля.
 Выводит на экран цифры числа, исключая нули. Отдельно выводится прописью номер позиции, с которой начинается эта серия."""
 import os
-if not os.path.isfile("text.txt"):
+if not os.path.isfile("D://Торрент//text.txt"):
     print("Файл не существует")
     exit()
 slv = {"0":"ноль", "1":"один", "2":"два", "3":"три", "4":"четыре", "5":"пять", "6":"шесть", "7":"семь", "8":"восемь", "9":"девять"}
 flag = 1
-with open ("text.txt", 'r') as f:
+with open ('D://Торрент//text.txt', 'r') as f:
     line = f.read()
     nmbrs = line.split()
 
+    print(nmbrs)
     print("Результат кода:")
     if nmbrs == []:
         flag = 2
     for i in range(len(nmbrs)):
         a = nmbrs[i]
+        notbn = int(a, 2)
         if not a.isdigit():
             continue
-        if int(a) % 2 != 0 or int(a) >= 1024:
+        if notbn % 2 != 0 or notbn >= 1024:
             continue
-        bn = bin(int(a))[2::]
         count = 0
 
-        while "000" in bn:
+        while "000" in a:
             count += 1
-            bn = bn.replace("000", "", 1)
+            a = a.replace("000", "", 1)
         if count >= 2:
-            print("Число номер", i,":", bn.replace("0", ""), end=". ")
+            print("Число номер", i,":", a.replace("0", ""), end=". ")
             flag = 0
 if flag == 1:
     print("В файле нет подходящий чисел")
